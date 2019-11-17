f65d3afaa376c4ca62c48421e6301ecf37456eac
Alex.Kourov <spam.magaziny@ya.ru>
2019-11-17 23:05:20 +0500

    Добавлены комментарии

--- 2_4 - orw_files/shop_list_from_cookbook.py
+++ 2_4 - orw_files/shop_list_from_cookbook.py
@@ -1,21 +1,23 @@
 import json
 
-cook_book = {}
+cook_book = {}  # Словарь для хранения всеё кники рецептов
 with open('recieps.txt', encoding='UTF8') as stream:
         while True:
-            dish = stream.readline().strip()
-            if not dish:
+            dish = stream.readline().strip()  # название блюда
+
+            if not dish:  # Проверяем его существование, иначе - прерываем цикл
                 break
             else:
-                cook_book[dish] = []
-            ingr_num = int(stream.readline())
-            # print(range(ingr_num))
+                cook_book[dish] = []  # НАзвание блюда становится ключом словаря
+
+            ingr_num = int(stream.readline())  #  Определяем, сколько раз ещё читаем строки, предже чем эта итерация закончится
+
             for ingr in range(ingr_num):
-                ingredient = {}
+                ingredient = {}  # Словарь хранящий данные об определённом ингридиенте
                 ingredient['ingridient_name'], ingredient['quantity'], ingredient['measure'] = stream.readline().strip().split(' | ')
-                cook_book[dish].append(ingredient)
+                cook_book[dish].append(ingredient)  # Помещаем данные об ингридиенте в блюдо cook_book[dish]
 
-            stream.readline()
+            stream.readline()  # Пропускаем пустую строку
 
 
-print(json.dumps(cook_book, ensure_ascii=False, indent=2))
+print(json.dumps(cook_book, ensure_ascii=False, indent=2))  # json используем для вывода в красивом виде
