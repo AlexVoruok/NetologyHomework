eaccc2392fc2276e9e9cf88ad924f0a7c0eaa400
Alex.Kourov <spam.magaziny@ya.ru>
2019-12-09 00:36:07 +0500

    Задача 1

---
+++ http_requests/3_2_http_requests_auto.py
@@ -0,0 +1,69 @@
+import requests
+#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
+
+API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+URL_for_lang_detect = 'https://translate.yandex.net/api/v1.5/tr.json/detect'
+
+def translate_it(*files_to_translate, to_lang='ru'):
+    """ Функция берёт каждый файл с текстом, указанный в параметре *files_to_translate, определяет
+    его язык, переводит на указанный в параметре to_lang язык и сохраняет в файл с названием
+    в формате from_<язык с которого переводим>_to_<язык на который переводим>.txt
+
+    параметры запроса для перевода:
+    URL = https://translate.yandex.net/api/v1.5/tr.json/translate ?
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+
+    параметры запроса для автоопределения языка:
+    https://translate.yandex.net/api/v1.5/tr.json/detect
+     ? [key=<API-ключ>]
+     & text=<текст>
+     & [hint=<список вероятных языков текста>]
+     & [callback=<имя callback-функции>]
+
+    :*files_to_translate: пути к файлам из которых из которого переводим
+    :to_lang: язык на который переводим
+
+    """
+
+    # цикл перебирает файлы, указанные в параметре files_to_translate
+    for file in files_to_translate:
+        with open(file, encoding='UTF8') as f:
+            text_to_translate = f.read()
+
+        # параметры для запроса на определение языка текста
+        detect_params = {
+            'key': API_KEY,
+            'text': text_to_translate
+        }
+
+        # запрос для определения языка текста
+        lang_detect_response = requests.get(URL_for_lang_detect, detect_params)
+        from_lang = lang_detect_response.json()['lang']
+
+        # параметры для запроса переводчику
+        params = {
+            'key': API_KEY,
+            'text': text_to_translate,
+            'lang': '{}-{}'.format(from_lang, to_lang),
+        }
+
+        # запрос переводчику
+        response = requests.get(URL, params=params)
+        json_ = response.json()
+
+        # записываем результат перевода в новый файл
+        with open(f'from_{from_lang}_to_{to_lang}.txt', 'w', encoding='UTF8') as ftw:
+            ftw.write(' '.join(json_['text']))
+
+        # сервисное сообщение для пользователя
+        print(f'Создан файл from_{from_lang}_to_{to_lang}.txt')
+
+
+if __name__ == '__main__':
+    translate_it('fr.txt', 'de.txt', 'es.txt', to_lang='ru')
