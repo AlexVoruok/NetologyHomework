4705d4f6c700c79c8fd1a7d2109d395e49f09e94
Alex.Kourov <spam.magaziny@ya.ru>
2019-12-17 02:04:00 +0500

    Обязательная часть задания выполнена

--- Netology_SPY-games/Netology_SPY_games.py
+++ Netology_SPY-games/Netology_SPY_games.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import time
+import json
 
 # имя пользователя (eshmargunov) и id (171691064) - являются валидными входными данными.
 access_token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
@@ -21,10 +22,13 @@ class User:
                           'access_token': access_token
                           }
 
-        friends_response = requests.get(URL + 'friends.get', friends_params)
-        friends_set = set(friends_response.json()['response']['items'])
-
-        return friends_set
+        try:
+            friends_response = requests.get(URL + 'friends.get', friends_params)
+            friends_set = set(friends_response.json()['response']['items'])
+            return friends_set
+        except KeyError:
+            error_msg = friends_response.json()['error']['error_msg']
+            pprint(error_msg)
 
     def get_user_groups(self):
         """ возвращает множество id групп пользователя
@@ -58,97 +62,84 @@ class User:
             else:
                 return error_msg
 
-    def user_url(self):
-        """ Возвращает ссылку на профиль пользователя
+    def __str__(self):
 
-        """
         url = f'https://vk.com/id{self.user_id}'
         return url
 
-    def common_friends_list(self, iduser2):
-        """ Принимает на вход id пользователя с которым требуется найти общих друзей
-        возвращает список общих друзей этих двух пользователей
-        где каждый элемент списка - экземпляр класса User
-        Выводит сообщение, если мы пытаемся сравнитьсписки пользователей с самим же собой
+    def unic_user_groups(self, friends_limit):
+        """ Выдаёт перечень уникальных групп пользователя
+            - в которых состоит он, но не состоит никто из его друзей
+            параметр friends_limit ограничивает количество друзей, с кем проиходит сравнение
 
         """
+        time.sleep(1)
 
-        # создадим экземляры класса
-        user1 = self
-        user2 = User(iduser2)
+        user_groups_set = self.get_user_groups()
+        user_friends_set = self.get_user_friends()
 
-        # выявим пересечение множеств друзей пользователей и сделаем из этого список
-        if self.user_id != user2.user_id:
-            common_friends = list(user1.get_user_friends() & user2.get_user_friends())
+        list_friends_instances = []
+        for friend in user_friends_set:
+            list_friends_instances.append(User(friend))
 
-            print(f'Количество общих друзей - {len(common_friends)}\n')
+        unic_user_groups = user_groups_set
 
-            # Создадим список общих друзей пользователей, состоящий из экземпляров класса User
-            list_common_friends_classes = []
-            for user in common_friends:
-                list_common_friends_classes.append(User(user))
-            return list_common_friends_classes
+        friend_counter = 0  # ограничивающий счётчик
 
-        else:
-            error = 'В качестве объекта для сравнениея вы ввели пользователя с тем же самым ID. \n' \
-                    'Для корректной работы функции common_friends_list требуется ввести ID другого пользователя'
-            return error
+        for friend in list_friends_instances:
+            friend_counter += 1
+            try:
+                # из множества групп пользователя убираем группы в которых состоит друг:
+                get_user_groups_method_respond = friend.get_user_groups()
+                unic_user_groups = unic_user_groups - get_user_groups_method_respond
+                print('. user c id', friend.user_id, 'обработан')
+            except TypeError:  # исключение вызывается когда пользователь ограничил доступ к своим группам
+                print(f'Нет информации от пользователя https://vk.com/id{friend.user_id} ',
+                      get_user_groups_method_respond)
+
+            if friend_counter == friends_limit:
+                break
+
+        print(f'\nПользователь {john} имеет {len(unic_user_groups)} уникальных групп(ы): \n'
+              + str(unic_user_groups) + '\n')
+
+        return unic_user_groups
+
+
+def get_group_info(group_id):
+    """ Принимает на вход один id группы или список, возвращает словарь следующего содержания:
+        “name”: “Название группы”,
+        “gid”: “идентификатор группы”,
+        “members_count”: количество_участников_сообщества
+
+    """
+
+    list_for_request = str(group_id)[1:-1]
+    list_to_return = []
+
+    gr_params = {'user_id': 3983782,
+                 'v': 5.61,
+                 'access_token': access_token,
+                 'group_ids': list_for_request,
+                 'fields': 'members_count'
+                 }
+
+    groups_response = requests.get(URL + 'groups.getById', gr_params)
+    for group in groups_response.json()['response']:
+        print('id: {}, Название: \n{}\nhttps://vk.com/{}\nКоличество пользователей: {}\n'.format(group['id'], group['name'], group['screen_name'], group['members_count'] ))
+        group_dict = {"name": group['name'], "gid": group['id'], "members_count": group['members_count']}
+        list_to_return.append(group_dict)
+
+    return list_to_return
 
 
 if __name__ == '__main__':
-    # создадим нашего тестового пользоваьеля
+
     john = User(171691064)
 
-    # множество id групп пользователя
-    user_set_groups = john.get_user_groups()
-    # pprint(user_set_groups)
+    unic_set = john.unic_user_groups(1000)
 
-    # Множество id друзей пользователя
-    user_friends_set = john.get_user_friends()
-    # pprint(user_friends_set)
+    dict_to_dump = get_group_info(unic_set)
 
-    # Создаём список друзей пользователя в виде экземпляров класса User
-    list_friends_instances = []
-    for friend in user_friends_set:
-        list_friends_instances.append(User(friend))
-    # pprint(list_friends_instances)
-
-    # Создадим множество уникальных групп пользователя от которого мы далее
-    # будем отсекать группы в которых состоят его друзья
-    unic_user_groups = user_set_groups
-
-    i = 0  # ограничивающий счётчик
-
-    # перебираем друзей пользователя
-    for friend in list_friends_instances:
-        i += 1
-        try:
-            # pprint(friend.get_user_groups())
-            # из множества групп пользователя убираем группы в которых состоит друг:
-            get_user_groups_method_respond = friend.get_user_groups()
-            unic_user_groups = unic_user_groups - get_user_groups_method_respond
-            print('. user c id', friend.user_id, 'обработан')
-        except TypeError:  # исключение вызывается когда пользователь ограничил доступ к своим группам
-            print(f'Нет информации от пользователя https://vk.com/id{friend.user_id} ', get_user_groups_method_respond)
-
-        # if i == 5:  # сервисный инструмент, который ограничивает число итераций
-        #     break
-
-    print(f'\nПеречень уникальных групп пользователя {john.user_url()}: \n', unic_user_groups)
-
-
-# Вывод информации о группах из перечня уникальных групп:
-    list_for_request = ''
-    for group in list(unic_user_groups):
-        list_for_request += str(group)+', '
-
-    pararams = {'user_id': 3983782,
-                'v': 5.61,
-                'access_token': access_token,
-                'group_ids': list_for_request,
-                }
-
-    groups_response = requests.get(URL + 'groups.getById', pararams)
-    for group in groups_response.json()['response']:
-        print('id: {}, Название: \n{}\nhttps://vk.com/{}\n'.format(group['id'], group['name'], group['screen_name']))
-
+    with open('groups.json', 'w', encoding='UTF-8') as ftw:
+        json.dump(dict_to_dump, ftw, ensure_ascii=False, indent=2)
